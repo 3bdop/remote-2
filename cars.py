51de57b2758dd36ff0d0d448b240c0f805e9d662
abood <abdbinm55@gmail.com>
2024-01-31 10:17:45 +0300

    New version of cars.py

--- cars.py
+++ cars.py
@@ -1,9 +1,9 @@
 import random
 
-cars = ["Honda","Toyota","GMC","Nissan","BMW"]
+cars = {"Honda":2015,"Toyota":2005,"GMC":2017,"Nissan":1999,"BMW":2022}
 
 def car():
-    print(random.choice(cars))
+    print("Year:",random.choice(list(cars.values())))
     
     
 if __name__=="__main__":
