61839382d4da9b305791cc95dbf1b2173b7549c6
abood <abdbinm55@gmail.com>
2024-01-31 10:02:10 +0300

    Updated version of cars.py

--- cars.py
+++ cars.py
@@ -1,5 +1,12 @@
-def car():
-    print("Vroom")
+import random
 
+cars = ["Honda","Toyota","GMC","Nissan","BMW"]
+
+def car():
+    # for i in range(len(cars)):
+    #     print(f"{i}. {cars[i]}")
+    print(random.choice(cars))
+    
+    
 if __name__=="__main__":
     car()
